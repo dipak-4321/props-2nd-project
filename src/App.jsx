05bf982f2d917ad163897c870bac30f8f2668015
dipak-4321 <dipakprince786@gmail.com>
2026-08-05 19:27:05 +0545

    heading has been done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Building } from 'lucide-react';
+function App(){
+  return (
+    <>
+    <div className="card">
+      <div className="header">
+        <h6 className="posCenter">subscribe</h6>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgwy1CidK3rqH2Vf3Fiv-f0DstoHzqDcPznaKZXxPOpQ&s=10" alt="" />
+        <h4>$45/hr</h4>
+      </div>
+
+
+    </div>
+    </>
+  )
+}
+export default App
