93c55c3b22db7835a57d2f998f9c446b897d36dc
dipak-4321 <dipakprince786@gmail.com>
2026-08-06 19:20:58 +0545

    center element has been added

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,22 @@ function App(){
     <>
     <div className="card">
       <div className="header">
-        <h6 className="posCenter">subscribe</h6>
+        <h6 className="posCenterR">subscribe</h6>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgwy1CidK3rqH2Vf3Fiv-f0DstoHzqDcPznaKZXxPOpQ&s=10" alt="" />
         <h4>$45/hr</h4>
+      </div>
+
+      <div className="center posCenterC ">
+        <h4>Tony Stark</h4>
+        <h5>UI/UX designer</h5>
+        <p className='detail '><Building className='detailbtn' /><span>Epic Coders</span></p>
+      </div>
+
+      <div className="boxbar posCenterR">
+        <h4>UI</h4>
+         <h4>UX</h4>
+         <h4>photoshop</h4>
+         <h4>+4</h4>
       </div>
 
 
